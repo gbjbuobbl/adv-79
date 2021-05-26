f1f6105bba151660b8f34a0d1b4ea5d85c9e35c9
gbjbuobbl <79749963+gbjbuobbl@users.noreply.github.com>
2021-05-26 15:47:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function submit(){ 
+    var guest = document.getElementById("names");
+names_of_people.push(guest)
+
+function addcomma(){
+    document.getElementById("namedisplayspan") = guest;
+    document.getElementById("namedisplaydivnonsorted") = guest;
+
+}
+function sort()
+{
+    names_of_people.sort(guest)
+    document.getElementById("sorteddivdisplay") = guest;
+}
+}
